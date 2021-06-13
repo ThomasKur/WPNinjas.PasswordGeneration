3edcbf07a114531e765327b2c7fcf091d3816528
Thomas Kurth <thomas@famkurth.ch>
2021-06-13 16:44:07 +0200

    Initial version

---
+++ WPNinjas.PasswordGeneration/PwService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WPNinjas.PasswordGeneration
+{
+	public class PwService
+	{
+		private static Int32 GetRandomInt()
+		{
+			byte[] randomBytes = new byte[4];
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+			rng.GetBytes(randomBytes);
+			Int32 randomInt = BitConverter.ToInt32(randomBytes, 0);
+			rng.Dispose();
+			return randomInt;
+		}
+
+		public static string GetRandomPassword(int len, string allowedCharacters = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz.:;,-_!?$%*=+&<>@#()23456789")
+		{
+			try
+			{
+				char[] possibleChars = allowedCharacters.ToCharArray();
+				if (possibleChars.Length < 1)
+				{
+					throw new Exception("You must enter one or more possible characters.");
+				}
+				if (len < 4)
+				{
+					throw new Exception($"Please choose a password length. That length must be a value between 4 and {Int32.MaxValue}. Note: values above 1,000 might take a LONG TIME to process on some computers.");
+				}
+
+				StringBuilder builder = new StringBuilder();
+
+				for (int i = 0; i < len; i++)
+				{
+					// Get our cryptographically random 32-bit integer & use as seed in Random class
+					// NOTE: random value generated PER ITERATION, meaning that the System.Random class
+					// is re-instantiated every iteration with a new, crytographically random numeric seed.
+					int randInt32 = GetRandomInt();
+					Random r = new Random(randInt32);
+
+					int nextInt = r.Next(possibleChars.Length);
+					char c = possibleChars[nextInt];
+					builder.Append(c);
+
+				}
+				// return final constructed string
+				return builder.ToString();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("An error has occurred while trying to generate random password!", ex);
+			}
+		}
+		public static SecureString GetRandomPasswordSecure(int len, string allowedCharacters = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz.:;,-_!?$%*=+&<>@#()23456789")
+		{
+			try
+			{
+				char[] possibleChars = allowedCharacters.ToCharArray();
+				if (possibleChars.Length < 1)
+				{
+					throw new Exception("You must enter one or more possible characters.");
+				}
+				if (len < 4)
+				{
+					throw new Exception($"Please choose a password length. That length must be a value between 4 and {Int32.MaxValue}. Note: values above 1,000 might take a LONG TIME to process on some computers.");
+				}
+
+				SecureString builder = new SecureString();
+
+				for (int i = 0; i < len; i++)
+				{
+					// Get our cryptographically random 32-bit integer & use as seed in Random class
+					// NOTE: random value generated PER ITERATION, meaning that the System.Random class
+					// is re-instantiated every iteration with a new, crytographically random numeric seed.
+					int randInt32 = GetRandomInt();
+					Random r = new Random(randInt32);
+
+					int nextInt = r.Next(possibleChars.Length);
+					char c = possibleChars[nextInt];
+					builder.AppendChar(c);
+
+				}
+				// return final constructed string
+				return builder;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("An error has occurred while trying to generate random password!", ex);
+			}
+		}
+	}
+}
